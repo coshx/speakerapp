24b49c1f560c304659cb8fde1ed8e9dd70e45dab
Chielo Zimmerman <chielo.zimmerman@gmail.com>
2013-02-14 18:22:23 -0500

    sync via server

--- public/js/speakerapp.js
+++ public/js/speakerapp.js
@@ -62,6 +62,7 @@ SpeakerApp = {
     $("#play").text("Pause");
     $("#sync").removeClass("disabled");
     app.audio.play();
+    //app.clockSyncExample() ;
     app.calculateSkew();
   },
   stop:function(){
@@ -71,81 +72,31 @@ SpeakerApp = {
     $("#sync").addClass("disabled");
     app.playing = false;
   },
-  measureLatency: function(){ //network + clock
-    var app = SpeakerApp;
-    app.latencyCount++ ;
-    var runtime_offset = 2 ;
-    var local_time1 = new Date().getTime() ;
-    var server_time_response ;
-    $.ajax({ url:  "/time", dataType: 'json', async: false, success: function(data) {
-        server_time_response = data["time"] ;}});
-    var local_time2 = new Date().getTime() ;
-    app.latency += local_time2 - local_time1 - runtime_offset ;
-    if (app.latencyCount < 100) {
-       app.calculateLatency();
-    }else{
-        console.log(app.latency/app.latencyCount ) ;
-    }
-  },
   serverTime:function(){
-    var runtime_offset = 2 ;
+    var app = SpeakerApp;
     var local_time1 = new Date().getTime() ;
     var server_time_response ;
     $.ajax({ url:  "/time", dataType: 'json', async: false, success: function(data) {
       server_time_response = data["time"] ;}});
-    var local_time2 = new Date().getTime() ;
-    var latency = local_time2 - local_time1 - runtime_offset ;
-    var server_time = server_time_response + latency ;
-    return server_time ;
-  },
-  clockSyncExample:function(){
-    var app = SpeakerApp;
-    var local_time1 = new Date().getTime() ;
-    var server_time_response ;
-      $.ajax({ url:  "/time", dataType: 'json', async: false, success: function(data) {
-         server_time_response = data["time"] ;}});
-    var local_time2 = new Date().getTime() ;
-    app.latency = local_time2 - local_time1;
-    var runtime = local_time1 - server_time_response+app.latency
-    var lag = app.latency+runtime-7
-    var server_time = server_time_response+lag ;
-    var server_offset  = server_time%1000 ;
-    var server_offset_change = server_offset - app.last_server_offset
-
-      if(server_offset > 500){
-        if(server_offset_change > 0)
-          local_offset = -10 ;
-      }if(server_offset < 500){
-        if(server_offset_change < 0)
-          local_offset = 10 ;
-      }
-      app.last_server_offset = server_offset ;
-
-      console.log(server_offset)   ;
-      setTimeout(app.calculateSkew, 1000+local_offset);
+    return 2*server_time_response - local_time1 ;
   },
   calculateSkew: function() {
     var app = SpeakerApp;
-    app.audio.volume = 0 ;
-
-
-    app.clockSyncExample() ;
-
-    return false ;
+    app.skewCount++ ;
 
     var ms = (app.serverTime()) % (1000*60*60);
     var newTime = ((ms) % (app.audio.duration*1000)) / 1000.0;
 
     app.skew += newTime - app.audio.currentTime;
 
-    if (app.skewCount % 20 == 0) {   //for some reason it seems to work better like this when reading from server
-      app.skew = app.skew / 20;      // "
+    if (app.skewCount % 10 == 0) {
+      app.skew = app.skew / 10;
+      console.log(app.skew) ;
       app.audio.currentTime = app.audio.currentTime + app.skew;
-
       app.skew = 0;
     }
 
-    if (app.skewCount == 20) {   //for some reason it seems to work better like this when reading from server
+    if (app.skewCount == 30) {
       app.skewCount = 0;
       app.audio.volume = 1;
     } else {
