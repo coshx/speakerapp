f90d7423c26bce5983649fd6d7c3cfbb6c4ed68e
Chielo Zimmerman <chielo.zimmerman@gmail.com>
2013-02-14 16:59:33 -0500

    adds clock sync example

--- public/js/speakerapp.js
+++ public/js/speakerapp.js
@@ -18,6 +18,11 @@ SpeakerApp = {
     app.latencyCount = 0 ;
     app.syncInterval = 30;
 
+
+    app.server_offset =0;
+    app.last_server_offset = 0;
+    app.local_offset = 0 ;
+
     app.audio = new Audio();
     app.audio.preload = "auto";
     app.audio.loop = true;
@@ -52,12 +57,12 @@ SpeakerApp = {
   play:function(){
     var app = SpeakerApp;
     app.audio.volume = 0;
-    app.audio.play();
     app.playing = true;
     app.skewCount = app.skew = 0;
-    app.calculateSkew();
     $("#play").text("Pause");
     $("#sync").removeClass("disabled");
+    app.audio.play();
+    app.calculateSkew();
   },
   stop:function(){
     var app = SpeakerApp;
@@ -93,10 +98,40 @@ SpeakerApp = {
     var server_time = server_time_response + latency ;
     return server_time ;
   },
+  clockSyncExample:function(){
+    var app = SpeakerApp;
+    var local_time1 = new Date().getTime() ;
+    var server_time_response ;
+      $.ajax({ url:  "/time", dataType: 'json', async: false, success: function(data) {
+         server_time_response = data["time"] ;}});
+    var local_time2 = new Date().getTime() ;
+    app.latency = local_time2 - local_time1;
+    var runtime = local_time1 - server_time_response+app.latency
+    var lag = app.latency+runtime-7
+    var server_time = server_time_response+lag ;
+    var server_offset  = server_time%1000 ;
+    var server_offset_change = server_offset - app.last_server_offset
+
+      if(server_offset > 500){
+        if(server_offset_change > 0)
+          local_offset = -10 ;
+      }if(server_offset < 500){
+        if(server_offset_change < 0)
+          local_offset = 10 ;
+      }
+      app.last_server_offset = server_offset ;
+
+      console.log(server_offset)   ;
+      setTimeout(app.calculateSkew, 1000+local_offset);
+  },
   calculateSkew: function() {
     var app = SpeakerApp;
     app.audio.volume = 0 ;
-    app.skewCount++;
+
+
+    app.clockSyncExample() ;
+
+    return false ;
 
     var ms = (app.serverTime()) % (1000*60*60);
     var newTime = ((ms) % (app.audio.duration*1000)) / 1000.0;
