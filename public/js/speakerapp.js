5549bd75abd52585fbf12a5101c14c1985793262
Chielo Zimmerman <chielo.zimmerman@gmail.com>
2013-02-14 19:57:22 -0500

    adds server logging of start time

--- public/js/speakerapp.js
+++ public/js/speakerapp.js
@@ -12,7 +12,7 @@ SpeakerApp = {
     var app = this;
     app.audio_loaded = false;
     app.playing = false;
-    app.skew = 0;
+    app.offset = 0;
     app.skewCount = 0;
     app.latency = 0 ;
     app.latencyCount = 0 ;
@@ -49,6 +49,7 @@ SpeakerApp = {
       } else if ($('#play').hasClass('disabled')) {
         // do nothing
       } else {
+        app.stop() ;
         app.play() ;
       }
       e.preventDefault();
@@ -58,7 +59,7 @@ SpeakerApp = {
     var app = SpeakerApp;
     app.audio.volume = 0;
     app.playing = true;
-    app.skewCount = app.skew = 0;
+    app.skewCount = app.offset = 0;
     $("#play").text("Pause");
     $("#sync").removeClass("disabled");
     app.audio.play();
@@ -74,33 +75,41 @@ SpeakerApp = {
   },
   serverTime:function(){
     var app = SpeakerApp;
-    var local_time1 = new Date().getTime() ;
+    var local_time = new Date().getTime() ;
     var server_time_response ;
     $.ajax({ url:  "/time", dataType: 'json', async: false, success: function(data) {
       server_time_response = data["time"] ;}});
-    return 2*server_time_response - local_time1 ;
+    return 2*server_time_response - local_time ;  //real server time estimate
   },
   calculateSkew: function() {
     var app = SpeakerApp;
     app.skewCount++ ;
 
-    var ms = (app.serverTime()) % (1000*60*60);
+    server_time = app.serverTime()
+    var ms = (server_time) % (1000*60*60);
     var newTime = ((ms) % (app.audio.duration*1000)) / 1000.0;
 
-    app.skew += newTime - app.audio.currentTime;
+    app.offset += newTime - app.audio.currentTime;
 
-    if (app.skewCount % 10 == 0) {
-      app.skew = app.skew / 10;
-      console.log(app.skew) ;
-      app.audio.currentTime = app.audio.currentTime + app.skew;
-      app.skew = 0;
+    if (app.skewCount == 10) {
+      app.offset = 0;
     }
 
-    if (app.skewCount == 30) {
+    if(app.skewCount < 110) {
+      setTimeout(app.calculateSkew, app.syncInterval);
+    } else {
+      app.offset = app.offset / 100;
+      app.audio.currentTime = app.audio.currentTime + app.offset;
+      var local_time = new Date().getTime() ;
+      console.log(local_time+" "+app.audio.currentTime) ;
+      $.ajax({
+        type: "POST",
+        url: "/post_start_time",
+        data: {local_time: local_time, audio_position: app.audio.currentTime},
+        dataType:  'json'
+        });
       app.skewCount = 0;
       app.audio.volume = 1;
-    } else {
-      setTimeout(app.calculateSkew, app.syncInterval);
     }
   }
 };
--- time.rb
+++ time.rb
@@ -10,3 +10,7 @@ get '/time' do
     { :time => (Time.now.utc.to_f * 1000.0).to_i}.to_json
 end
 
+post '/post_start_time' do
+  logger.info("{local_time: #{params[:local_time]}, audio_position: #{params[:audio_position]}" )
+end
+
