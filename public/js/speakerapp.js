0ac953afeeb6cafe89d06a4ee32073ddfd988c54
Chielo Zimmerman <chielo.zimmerman@gmail.com>
2013-02-15 17:01:00 -0500

    causes ongoing sync

--- public/js/speakerapp.js
+++ public/js/speakerapp.js
@@ -1,117 +1,61 @@
-SpeakerApp = {
+speakerApp = {
   init: function() {
     var app = this;
-    app.time = 0;
     $(function() {
       app.initAudio();
-      app.initPlayHandler();
     });
   },
-
   initAudio: function() {
-    var app = this;
-    app.audio_loaded = false;
-    app.playing = false;
-    app.offset = 0;
-    app.skewCount = 0;
-    app.latency = 0 ;
-    app.latencyCount = 0 ;
-    app.syncInterval = 30;
-
-
-    app.server_offset =0;
-    app.last_server_offset = 0;
-    app.local_offset = 0 ;
-
+    var app = speakerApp;
     app.audio = new Audio();
-    app.audio.preload = "auto";
-    app.audio.loop = true;
+    app.audio.volume = 0;
     app.audio.src = "media/rainbow.mp3";
+    app.syncInterval_short = 500;
+    app.syncInterval_long = 2000;
+    app.milliseconds_in_a_year = 1000*60*60 ;
+    app.current_audio_position = false ;
+    app.playing = false ;
+    app.counter = 0 ;
 
-    $(app.audio).on("canplaythrough", function() {
-      $("#play").removeClass("disabled");
-       app.audio_loaded = true;
-      });
-  },
-
-  initPlayHandler: function() {
-    var app = this;
-    $('#sync').on('click', function(e) {
-      if(!$('#play').hasClass('disabled')){
-        app.stop() ;
-        app.play() ;
-      }
-      e.preventDefault();
-    }) ;
     $('#play').on('click', function(e) {
-      if (app.playing) {
-        app.stop() ;
-      } else if ($('#play').hasClass('disabled')) {
-        // do nothing
-      } else {
-        app.stop() ;
-        app.play() ;
-      }
-      e.preventDefault();
+      var app = speakerApp;
+      app.audio.volume = 0 ;
+      app.audio.play();
+      app.calculateSkew() ;
+      $("#play").addClass("disabled");
     });
   },
-  play:function(){
-    var app = SpeakerApp;
-    app.audio.volume = 0;
-    app.playing = true;
-    app.skewCount = app.offset = 0;
-    $("#play").text("Pause");
-    $("#sync").removeClass("disabled");
-    app.audio.play();
-    //app.clockSyncExample() ;
-    app.calculateSkew();
+  serverLog:function(local_time, current_position){
+    var app = speakerApp;
+    $.ajax({
+      type: "POST",
+      url: "/post_start_time",
+      data: {local_time: local_time, audio_position: current_position},
+      dataType:  'json'
+    });
   },
-  mute:function(){
-    var app = SpeakerApp;
-    app.audio.pause();
-    $("#play").text("Play");
-    $("#sync").addClass("disabled");
-    app.playing = false;
-  },
-  serverTime:function(){
-    var app = SpeakerApp;
-    var local_time = new Date().getTime() ;
-    var server_time_response ;
+  skewSeek:function(){
+    var app = speakerApp;
+    app.begin_time = new Date().getTime() ;
     $.ajax({ url:  "/time", dataType: 'json', async: false, success: function(data) {
-      server_time_response = data["time"] ;}});
-    return 2*server_time_response - local_time ;  //real server time estimate
+      app.server_time_response = data["time"] ;
+    }});
+    app.time = new Date().getTime() ;
+    app.skew = app.time - app.server_time_response ;
+    app.audio.currentTime  = ((app.server_time_response % app.milliseconds_in_a_year) % app.song_duration) * 0.001 ;
+    app.audio.volume = 1 ;
+    app.serverLog(app.time, app.current_audio_position) ;
+    setTimeout(app.skewSeek,app.syncInterval_long) ;
   },
   calculateSkew: function() {
-    var app = SpeakerApp;
-    app.skewCount++ ;
-
-    server_time = app.serverTime()
-    var ms = (server_time) % (1000*60*60);
-    var newTime = ((ms) % (app.audio.duration*1000)) / 1000.0;
-
-    app.offset += newTime - app.audio.currentTime;
-
-    if (app.skewCount == 10) {
-      app.offset = 0;
-    }
-
-    if(app.skewCount < 110) {
-      setTimeout(app.calculateSkew, app.syncInterval);
-    } else {
-      app.offset = app.offset / 100;
-      app.audio.currentTime = app.audio.currentTime + app.offset;
-      var local_time = new Date().getTime() ;
-      console.log(local_time+" "+app.audio.currentTime) ;
-      $.ajax({
-        type: "POST",
-        url: "/post_start_time",
-        data: {local_time: local_time, audio_position: app.audio.currentTime},
-        dataType:  'json'
-        });
-      app.skewCount = 0;
-      app.audio.volume = 1;
+    var app = speakerApp;
+    if(app.audio.currentTime==0){ //(fix for mobile safari block on pre-loading - 'waits til it starts playing')
+     setTimeout(app.calculateSkew, 500);
+    }else{
+     app.song_duration = app.audio.duration*1000 ;
+     app.skewSeek() ;
     }
   }
 };
 
-SpeakerApp.init();
+speakerApp.init();
--- time.rb
+++ time.rb
@@ -6,11 +6,12 @@ get "/" do
 end
 
 get '/time' do
-  content_type :json
+   content_type :json
     { :time => (Time.now.utc.to_f * 1000.0).to_i}.to_json
 end
 
 post '/post_start_time' do
   logger.info("{local_time: #{params[:local_time]}, audio_position: #{params[:audio_position]}" )
 end
+
 
